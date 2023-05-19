3bd5d6e404f1ebd788222ce97cbcb83c3ee48728
Fernando Augusto <fams@linuxplace.com.br>
2023-05-19 12:56:12 -0300

    Capturing body

--- kong-plugin-log-gelf/kong/plugins/log-gelf/gelfEncoder.lua
+++ kong-plugin-log-gelf/kong/plugins/log-gelf/gelfEncoder.lua
@@ -1,79 +1,90 @@
-local cjson = require "cjson"
-local socket = require "socket"
+local cjson = require "cjson"   -- Import the cjson module for JSON encoding/decoding
+-- local socket = require "socket" -- Import the socket module for time functions
+local ffi = require "ffi"
 
-local GELF_VERSION = "1.1"
+-- local bit = require "bit"
 
-local GELF_MAGICK = string.char(0x1e) .. string.char( 0x0f)
 
-local _M = {}
+local GELF_VERSION = "1.1" -- Constant for the GELF version
 
-local function get_id()
-    local buf = {}
-    local timestamp_mili = socket.gettime()*1000
-    ngx.log(ngx.DEBUG, "timestamp_mili:" .. timestamp_mili)
-    for i = 1, 8 do
-        buf[#buf+1] = string.format('%x', math.random(0, 0xf))
-    end
-    return table.concat(buf)
+local GELF_MAGICK = string.char(0x1e) .. string.char(0x0f) -- Constant for the GELF magic string
+
+local _M = {} -- Module table to hold functions and data
+
+
+-- Define the C data types for uint64_t and uint8_t
+ffi.cdef[[
+    typedef uint64_t uint64_t;
+    typedef uint8_t uint8_t;
+]]
+
+
+function get_id()
+    -- Generate a random 64-bit unsigned integer
+    local id = ffi.cast("uint64_t", math.random(0, 0xFFFFFFFFFFFFFFFF))
+  
+    -- Convert the integer to an 8-byte string
+    local id_str = ffi.string(ffi.cast("uint8_t*", ffi.new("uint64_t[1]", id)), 8)
+  
+    return id_str
 end
 
 function _M:new(conf, host, level)
-    self.msg = {}
-    self.conf = conf
-    self.chunk_data_size = conf.chunk_size-12
-    self.msg.version = GELF_VERSION
-    self.msg.host = host
-    self.msg.level = level
-    self.parsed = nil
-    self.id = get_id()
-    return self
+    self.msg = {} -- Create a table to hold the log message
+    self.conf = conf -- Store the provided configuration
+    self.chunk_data_size = conf.chunk_size - 12 -- Calculate the chunk data size based on the configuration
+    self.msg.version = GELF_VERSION -- Set the GELF version in the log message
+    self.msg.host = host -- Set the host in the log message
+    self.msg.level = level -- Set the log level in the log message
+    self.parsed = nil -- Initialize the parsed JSON string to nil
+    self.id = get_id() -- Generate an ID for the log message
+    return self -- Return the initialized object
 end
 
 function _M:set_message(short_message, full_message)
-    self.msg.short_message = short_message
-    self.msg.full_message = full_message
-    self.parsed = nil
+    self.msg.short_message = short_message -- Set the short log message
+    self.msg.full_message = full_message -- Set the full log message
+    self.parsed = nil -- Reset the parsed JSON string
 end
 
 function _M:set_additional_fields(fields)
-    if #fields > 1 then
-        for k,v in pairs(fields) do
-            self.msg["_".. k] = v
+    if #fields > 1 then -- Check if additional fields are provided
+        for k, v in pairs(fields) do
+            self.msg["_" .. k] = v -- Add each additional field to the log message, prefixed with an underscore
         end
     end
-    self.parsed = nil
+    self.parsed = nil -- Reset the parsed JSON string
 end
 
 function _M:chunk(count)
     if not self.parsed then
-        self.parsed = cjson.encode(self.msg)
+        self.parsed =  cjson.encode(self.msg) -- Encode the log message table into a JSON string if not already done
     end
-    
-    --[[
-    chunkSize        int
-    chunkDataSize    int
-    compressionType  int
-    compressionLevel int
-    --]]
-    
-    if #self.parsed < self.chunk_data_size  then
-        return 0, self.parsed
-    end
-    ngx.log(ngx.DEBUG,"Sending chunk: " .. count)
 
-    local total = math.ceil(#self.parsed / self.chunk_data_size)
-    if total > self.conf.chunk_max_number then error("Message chunks exceeded") end
-    ngx.log(ngx.DEBUG, "Total: " .. total .. " = math.ceil(" .. #self.parsed .. " / " .. self.chunk_data_size .. ")")
+
+    if #self.parsed < self.chunk_data_size then
+        return 0, self.parsed -- Return a single chunk with count 0 if the message size is smaller than the chunk data size
+    end
+
+    ngx.log(ngx.DEBUG, "Sending chunk: " .. count) -- Log the chunk count being sent
+
+    local total = math.ceil(#self.parsed / self.chunk_data_size) -- Calculate the total number of chunks required
+    if total > self.conf.chunk_max_number then
+        error("Message chunks exceeded") -- Check if the total number of chunks exceeds the configured maximum and raise an error if so
+    end
+    ngx.log(ngx.DEBUG, "Total: " .. total .. "= math.ceil(" .. #self.parsed .. " / " .. self.chunk_data_size .. ")") -- Log the total number of chunks being calculated
+
+    local next_chunk = count + 1 -- Calculate the index of the next chunk
+    ngx.log(ngx.DEBUG, "Next chunk" .. next_chunk) -- Log the index of the next chunk
     
-    local next_chunk = count + 1
-    ngx.log(ngx.DEBUG, "Next chunk" .. next_chunk)
-    return  (total - next_chunk ) == 0 and 0 or next_chunk,
-        GELF_MAGICK .. self.id .. string.char(count) .. string.char(total) 
-            .. string.sub(
-                self.parsed,
-                count*self.chunk_data_size,
-                (count+1)*self.chunk_data_size
-            )
+    -- Construct the chunk payload by appending the GELF magic string, ID, count, total, and a substring of the JSON string based on the chunk index
+    return (total - next_chunk) == 0 and 0 or next_chunk,
+        GELF_MAGICK .. self.id .. string.char(count) .. string.char(total) ..
+        string.sub(
+            self.parsed,
+            count * self.chunk_data_size,
+            (count + 1) * self.chunk_data_size
+        )
 end
 
-return _M
+return _M -- Return the module table as the final result
--- kong-plugin-log-gelf/kong/plugins/log-gelf/handler.lua
+++ kong-plugin-log-gelf/kong/plugins/log-gelf/handler.lua
@@ -8,13 +8,13 @@ local sandbox = require "kong.tools.sandbox".sandbox
 local kong = kong
 local ngx = ngx
 local timer_at = ngx.timer.at
-local udp = ngx.socket.udp
+-- local udp = ngx.socket.udp
 
-local ngx_log_ERR = ngx.ERR
-local ngx_log_INFO = ngx.INFO
-local ngx_timer_at = ngx.timer.at
-local string_format = string.format
-local ngx_timer_every = ngx.timer.every
+-- local ngx_log_ERR = ngx.ERR
+-- local ngx_log_INFO = ngx.INFO
+-- local ngx_timer_at = ngx.timer.at
+-- local string_format = string.format
+-- local ngx_timer_every = ngx.timer.every
 
 local LogGelf = {
   VERSION = "0.0.1-1",
@@ -22,35 +22,33 @@ local LogGelf = {
 }
 
 
-local sandbox_opts = { env = { kong = kong, ngx = ngx } }
-local my_message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+-- local sandbox_opts = { env = { kong = kong, ngx = ngx } }
+-- local my_message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 
 function LogGelf:access(conf)
-    if is_json_body(kong.request.get_header("Content-Type")) then
-      local ctx = kong.ctx.gelf_log;
-      ctx.request_body = kong.request.get_raw_body();
+  local content_length = kong.request.get_header("Content-lenght")
+  local req_body = kong.request.get_body()
+  if conf.capture_request_body and (content_length == nil and string.len(req_body) and tonumber(content_length) <= conf.request_max_body_size_limit) or (content_length ~= nil and tonumber(content_length) <= conf.request_max_body_size_limit) then
+     if req_body then
+    -- Storing the request body in kong.ctx shared context
+      kong.ctx.plugin.request_body = req_body
     end
+  else
+    kong.ctx.plugin.request_body = ""
+    kong.ctx.plugin.request_body_size_exceeded = true
+  end
 end
 
 function LogGelf:log(conf)
-    -- if conf.custom_fields_by_lua then
-    --   local set_serialize_value = kong.log.set_serialize_value
-    --   for key, expression in pairs(conf.custom_fields_by_lua) do
-    --     set_serialize_value(key, sandbox(expression, sandbox_opts)())
-    --   end
-    -- end
-    local buf = {}
-    for i = 1, 300 do
-        buf[#buf+1] = my_message
-    end
-    local final_string = table.concat( buf )
-    local gelf_message = gelfEncoder:new(conf,kong.node.get_hostname(), 1)
-    gelf_message:set_message("short",final_string)
-    gelf_message:set_additional_fields(kong.log.serialize())
-    local ok, err = timer_at(0, log.log, conf, gelf_message)
-    if not ok then
-      kong.log.err("could not create timer: ", err)
-    end
+  local final_string = kong.ctx.plugin.request_body
+  local gelf_message = gelfEncoder:new(conf,kong.node.get_hostname(), 1)
+  gelf_message:set_message("short",final_string)
+  gelf_message:set_additional_fields(kong.log.serialize())
+  local ok, err = timer_at(0, log.log, conf, gelf_message)
+  if not ok then
+    kong.log.err("could not create timer: ", err)
+  end
 end
 
 return LogGelf
